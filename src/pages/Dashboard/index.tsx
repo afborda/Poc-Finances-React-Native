5e5c9786605ad89384675284d398a6f58290cd63
ABNER BORDA FONSECA <abner.fonseca@C02DX4KNMD6P.local>
2021-11-28 17:25:31 -0300

    erro flatlist type

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,116 @@
+import React from "react";
+
+import HighlightCard from "../../components/HighlightCard";
+import TransactionCard, {
+  TransactionCardProps,
+} from "../../components/TransactionCard";
+import {
+  Container,
+  Header,
+  UserContainer,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+  HighlightCards,
+  Transactions,
+  Title,
+  TransactionsList,
+} from "./styles";
+
+export interface DataListProps extends TransactionCardProps {
+  id: string;
+}
+
+const Dashboard = () => {
+  const data: DataListProps = [
+    {
+      id: "1",
+      type: "positive",
+      title: "Desenvolvimento de site",
+      amount: "R$ 12.000,00",
+      category: {
+        name: "Vendas",
+        icon: "dollar-sign",
+      },
+      date: "13/04/2020",
+    },
+    {
+      id: "2",
+      type: "negative",
+      title: "Desenvolvimento de Bonecos",
+      amount: "R$ 12.000,00",
+      category: {
+        name: "Vendas",
+        icon: "dollar-sign",
+      },
+      date: "13/04/2020",
+    },
+    {
+      id: "3",
+      type: "positive",
+      title: "Desenvolvimento de Aplicativo",
+      amount: "R$ 12.000,00",
+      category: {
+        name: "Vendas",
+        icon: "dollar-sign",
+      },
+      date: "13/04/2020",
+    },
+  ];
+
+  return (
+    <Container>
+      <Header>
+        <UserContainer>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/14080449?v=4",
+              }}
+            />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Abner Fonseca</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserContainer>
+      </Header>
+
+      <HighlightCards>
+        <HighlightCard
+          type="up"
+          title="Entradas"
+          amount="R$ 1.259,00"
+          lastTransaction="Última saída dia 03 de abril"
+        />
+        <HighlightCard
+          type="down"
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lastTransaction="Última saída dia 03 de abril"
+        />
+        <HighlightCard
+          type="total"
+          title="Total"
+          amount="R$ 16.141,00"
+          lastTransaction="01 à 16 de abril"
+        />
+      </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+        <TransactionsList
+          data={data}
+          keyExtractor={(item: DataListProps) => item.id}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+        />
+      </Transactions>
+    </Container>
+  );
+};
+
+export default Dashboard;
